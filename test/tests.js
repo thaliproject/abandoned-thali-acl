6fdd82149f9c6eaf25baf5ed1e8357d292ad9660
Sreejumon <purayil@microsoft.com>
2015-05-14 11:18:22 -0700

    Added AddRole, RemoveRole test cases

--- test/tests.js
+++ test/tests.js
@@ -1,17 +1,76 @@
 var assert = require("assert");
 var ThaliAclDb = require('../.');
 
+//Initialize the ACL db
+var   db = new ThaliAclDb('acl', { db: require('memdown') });
+
+describe ("Initialize ACL", function(){
+    it("Create Thali ACL db", function(done){
+        var   db = new ThaliAclDb('acl', { db: require('memdown') });
+        done();
+    });
+});
+
+
 describe("Add roles", function(){
-  it("admin, guest, member roles", function(done){
-    var  db = new ThaliAclDb('acl', { db: require('memdown') });
-    db.addRole('admin')
+  it("Add admin role", function(done) {
+      db.addRole('admin')
+          .then(function () {
+              assert(true);
+              done();
+          })
+          .catch(function () {
+              assert(false);
+              done();
+          });
+  });
+  it("Add guest role", function(done){
+  db.addRole('guest')
       .then(function() {
-        assert(true);
-        done();
+          assert(true);
+          done();
       })
-      .catch(function (err) {
-        assert(false);
-        done();
+      .catch(function () {
+          assert(false);
+          done();
       });
   });
+});
+
+describe("Remove roles", function(){
+    it("Remove admin role", function(done) {
+        db.removeRole('admin')
+            .then(function () {
+                assert(true);
+                done();
+            })
+            .catch(function () {
+                assert(false);
+                done();
+            });
+    });
+    it("Remove guest role", function(done){
+        db.removeRole('guest')
+            .then(function() {
+                assert(true);
+                done();
+            })
+            .catch(function () {
+                assert(false);
+                done();
+            });
+    });
+
+    //TODO: Should this test fail, since there is no more guest role?
+    it("Remove guest role", function(done){
+        db.removeRole('guest')
+            .then(function() {
+                assert(true);
+                done();
+            })
+            .catch(function () {
+                assert(false);
+                done();
+            });
+    });
 });
