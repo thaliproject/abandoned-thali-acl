00c4b00044f285f6a1389b2d47c6defe4d628ffa
sreesharp <purayil@microsoft.com>
2015-05-14 17:49:36 -0700

    Added more mocha tests

--- test/tests.js
+++ test/tests.js
@@ -13,7 +13,7 @@ describe ("Initialize ACL", function(){
 
 
 describe("Add roles", function(){
-  it("Add admin role", function(done) {
+    it("Add admin role", function(done) {
       db.addRole('admin')
           .then(function () {
               assert(true);
@@ -23,9 +23,9 @@ describe("Add roles", function(){
               assert(false);
               done();
           });
-  });
-  it("Add guest role", function(done){
-  db.addRole('guest')
+    });
+    it("Add guest role", function(done){
+    db.addRole('guest')
       .then(function() {
           assert(true);
           done();
@@ -34,8 +34,10 @@ describe("Add roles", function(){
           assert(false);
           done();
       });
-  });
+    });
 });
+
+
 
 describe("Remove roles", function(){
     it("Remove admin role", function(done) {
@@ -49,19 +51,7 @@ describe("Remove roles", function(){
                 done();
             });
     });
-    it("Remove guest role", function(done){
-        db.removeRole('guest')
-            .then(function() {
-                assert(true);
-                done();
-            })
-            .catch(function () {
-                assert(false);
-                done();
-            });
-    });
 
-    //TODO: Should this test fail, since there is no more guest role?
     it("Remove guest role", function(done){
         db.removeRole('guest')
             .then(function() {
@@ -74,3 +64,176 @@ describe("Remove roles", function(){
             });
     });
 });
+
+
+describe("Remove non-existing/invalid roles", function(){
+    it("Remove manager role should fail", function(done) {
+        db.removeRole('manager')
+            .then(function () {
+                assert(false)
+                done();
+             })
+            .catch(function (err) {
+                done(err);
+            });
+    });
+
+    //TODO: Are we allowed to remove same roles multiple times?
+    it("Remove guest role (which is already removed) should fail", function(done){
+        db.removeRole('guest')
+            .then(function() {
+                console.log("guest role removed");
+                assert(false);
+                done();
+            })
+            .catch(function (err) {
+                done(err);
+            });
+        done();
+    });
+});
+
+
+describe("Add user to roles", function(){
+    it("Add user thali to admin role", function(done) {
+        db.addRole('admin')
+            .then(function () {
+                return db.addUserToRole("thali","admin");
+            })
+            .then(function () {
+                assert(true);
+                done();
+            })
+            .catch(function () {
+                assert(false);
+                done();
+            });
+    });
+    it("Add user sam to guest role", function(done){
+        db.addRole('guest')
+            .then(function() {
+                return db.addUserToRole("sam","guest");
+            })
+            .then(function () {
+                assert(true);
+                done();
+            })
+            .catch(function () {
+                assert(false);
+                done();
+            });
+    });
+});
+
+describe("Add user to invalid role", function(){
+    it("User sally should not add to manager(non-existing) role", function(done) {
+        db.addRole('admin')
+            .then(function () {
+                return db.addUserToRole("sally","manager");
+            })
+            .then(function () {
+                assert(false);
+                done();
+            })
+            .catch(function (err) {
+                done(err);
+            });
+    });
+});
+
+
+describe("Remove user from role", function() {
+    it("Remove user thali from admin role", function (done) {
+        db.addRole('admin')
+            .then(function () {
+                return db.addUserToRole("thali", "admin");
+            })
+            .then(function () {
+                return db.removeUserFromRole("thali","admin");
+            })
+            .then(function () {
+                assert(true);
+                done();
+            })
+            .catch(function () {
+                assert(false);
+                done();
+            });
+    });
+
+    it("Remove invalid user(James) from admin role should fail", function (done) {
+        db.addRole('admin')
+            .then(function () {
+                return db.removeUserFromRole("James","admin");
+            })
+            .then(function () {
+                assert(false);
+                done();
+            })
+            .catch(function (err) {
+                done(err);
+            });
+    });
+});
+
+describe("Get role by user", function() {
+    it("Role of user thali should be 'admin'", function (done) {
+        db.addRole('admin')
+            .then(function () {
+                return db.addUserToRole("thali", "admin");
+            })
+            .then(function () {
+                return db.getRolesByUser("thali");
+            })
+            .then(function (roles) {
+                assert.equal(roles,'admin');
+                done();
+            })
+            .catch(function (err) {
+                done(err);
+            });
+    });
+    //TODO: Review please
+    it("Role of user sam shouldn't be be 'admin'", function (done) {
+        db.addRole('guest')
+            .then(function () {
+                return db.addUserToRole("sam", "guest");
+            })
+            .then(function () {
+                return db.getRolesByUser("sam");
+            })
+            .then(function (roles) {
+                if(roles !== 'admin') {
+                    assert(true);
+                }
+                else{
+                    assert(false);
+                }
+                done();
+            })
+            .catch(function (err) {
+                done(err);
+            });
+    });
+});
+
+describe("Get users by role", function() {
+    it("Admin role should have one user - thali", function (done) {
+        db.addRole('admin')
+            .then(function () {
+                return db.addUserToRole("thali", "admin");
+            })
+            .then(function () {
+                return db.getUsersByRole("admin");
+            })
+            .then(function (users) {
+                assert.equal(users,'thali');
+                done();
+            })
+            .catch(function (err) {
+                done(err);
+            });
+    });
+
+});
+
