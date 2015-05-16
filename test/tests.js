665a3f4d55b2f05cf88184f0300dff7b991f0fdc
Matthew Podwysocki <matthew.podwysocki@gmail.com>
2015-05-16 19:29:59 +0200

    Removing bad tests

--- test/tests.js
+++ test/tests.js
@@ -2,160 +2,84 @@ var assert = require("assert");
 var ThaliAclDb = require('../.');
 
 //Initialize the ACL db
-var   db = new ThaliAclDb('acl', { db: require('memdown') });
+var db = new ThaliAclDb('acl', { db: require('memdown') });
 
 describe ("Initialize ACL", function(){
-    it("Create Thali ACL db", function(done){
-        var   db = new ThaliAclDb('acl', { db: require('memdown') });
-        done();
-    });
+  it("Create Thali ACL db", function(done){
+    var db = new ThaliAclDb('acl', { db: require('memdown') });
+    done();
+  });
 });
 
 
 describe("isAllowed", function(){
-    it("User Joy should be allowed to guest role", function(done) {
-        db.addRole('guest')
-            .then(function () {
-                return db.addUserToRole('joy', 'guest');
-            })
-            .then(function () {
-                return db.isAllowed('joy', 'guest');
-            })
-            .then(function (isAllowed) {
-                assert.equal(isAllowed,true);
-                done();
-            })
-            .catch(function(err) {
-               done(err);
-            });
-    });
-    it("User Joy should not be allowed to admin role", function(done) {
-        db.addRole('admin')
-            .then(function () {
-                return db.addUserToRole('joy', 'guest');
-            })
-            .then(function () {
-                return db.isAllowed('joy', 'admin');
-            })
-            .then(function (isAllowed) {
-                assert.equal(isAllowed,false);
-                done();
-            })
-            .catch(function(err) {
-                done(err);
-            });
-    });
+  it("User Joy should be allowed to guest role", function(done) {
+    db.addRole('guest')
+      .then(function () {
+        return db.addUserToRole('joy', 'guest');
+      })
+      .then(function () {
+        return db.isAllowed('joy', 'guest');
+      })
+      .then(function (isAllowed) {
+        assert.equal(isAllowed,true);
+        done();
+      })
+      .catch(done);
+  });
+  it("User Joy should not be allowed to admin role", function(done) {
+    db.addRole('admin')
+      .then(function () {
+        return db.addUserToRole('joy', 'guest');
+      })
+      .then(function () {
+        return db.isAllowed('joy', 'admin');
+      })
+      .then(function (isAllowed) {
+        assert.equal(isAllowed,false);
+        done();
+      })
+      .catch(done);
+  });
 });
 
 describe("Add roles", function(){
-    it("Add admin role", function(done) {
-      db.addRole('admin')
-          .then(function () {
-              assert(true);
-              done();
-          })
-          .catch(function () {
-              assert(false);
-              done();
-          });
-    });
-    it("Add guest role", function(done){
+  it("Add admin role", function(done) {
+    db.addRole('admin')
+      .then(function () { done(); }, done);
+  });
+  it("Add guest role", function(done){
     db.addRole('guest')
-      .then(function() {
-          assert(true);
-          done();
-      })
-      .catch(function () {
-          assert(false);
-          done();
-      });
-    });
+      .then(function() { done(); }, done);
+  });
 });
 
-
-
 describe("Remove roles", function(){
-    it("Remove admin role", function(done) {
-        db.removeRole('admin')
-            .then(function () {
-                assert(true);
-                done();
-            })
-            .catch(function () {
-                assert(false);
-                done();
-            });
+  it("Remove admin role", function(done) {
+    db.removeRole('admin')
+      .then(function () { done(); }, done);
     });
 
     it("Remove guest role", function(done){
-        db.removeRole('guest')
-            .then(function() {
-                assert(true);
-                done();
-            })
-            .catch(function () {
-                assert(false);
-                done();
-            });
+      db.removeRole('guest')
+          .then(function () { done(); }, done);
     });
 });
-
-
-describe("Remove non-existing/invalid roles", function(){
-    it("Remove manager role should fail", function(done) {
-        db.removeRole('manager')
-            .then(function () {
-                assert(false)
-                done();
-             })
-            .catch(function (err) {
-                done(err);
-            });
-    });
-
-    //TODO: Are we allowed to remove same roles multiple times?
-    it("Remove guest role (which is already removed) should fail", function(done){
-        db.removeRole('guest')
-            .then(function() {
-                assert(false);
-                done();
-            })
-            .catch(function (err) {
-                done(err);
-            });
-        done();
-    });
-});
-
 
 describe("Add user to roles", function(){
-    it("Add user thali to admin role", function(done) {
-        db.addRole('admin')
-            .then(function () {
-                return db.addUserToRole("thali","admin");
-            })
-            .then(function () {
-                assert(true);
-                done();
-            })
-            .catch(function () {
-                assert(false);
-                done();
-            });
-    });
-    it("Add user sam to guest role", function(done){
-        db.addRole('guest')
-            .then(function() {
-                return db.addUserToRole("sam","guest");
-            })
-            .then(function () {
-                assert(true);
-                done();
-            })
-            .catch(function () {
-                assert(false);
-                done();
-            });
+  it("Add user thali to admin role", function(done) {
+    db.addRole('admin')
+      .then(function () {
+        return db.addUserToRole("thali","admin");
+      })
+      .then(function () { done(); }, done);
+  });
+  it("Add user sam to guest role", function(done){
+    db.addRole('guest')
+      .then(function() {
+        return db.addUserToRole("sam","guest");
+      })
+      .then(function () { done(); }, done);
     });
 });
 
@@ -252,22 +176,18 @@ describe("Get role by user", function() {
 });
 
 describe("Get users by role", function() {
-    it("Admin role should have one user - thali", function (done) {
-        db.addRole('admin')
-            .then(function () {
-                return db.addUserToRole("thali", "admin");
-            })
-            .then(function () {
-                return db.getUsersByRole("admin");
-            })
-            .then(function (users) {
-                assert.equal(users,'thali');
-                done();
-            })
-            .catch(function (err) {
-                done(err);
-            });
-    });
-
+  it("Admin role should have one user - thali", function (done) {
+    db.addRole('admin')
+      .then(function () {
+        return db.addUserToRole("thali", "admin");
+      })
+      .then(function () {
+        return db.getUsersByRole("admin");
+      })
+      .then(function (users) {
+        assert.equal(users,'thali');
+        done();
+      });
+      .catch(done);
+  });
 });
-
